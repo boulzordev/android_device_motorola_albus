7dce639f75f00e8df29c22340c9377f62f2a5398
Vachounet <vachounet@live.fr>
2017-08-29 12:15:21 +0200

    albus: build missing package
    Change-Id: Ie2ea61108730835d83105e644bad3a9cce33b9f7

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
+    libshim_adsp
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
@@ -252,6 +253,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 PRODUCT_PACKAGES += \
     libnfc \
     libnfc_jni \
@@ -300,15 +305,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl \
     libshim_ril \
-    libxml2
+    libprotobuf-cpp-full \
+    libxml2 \
+    rild_socket
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
-#PRODUCT_PACKAGES += \
-#    android.hardware.sensors@1.0-impl
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
 
 # Thermal
 PRODUCT_COPY_FILES += \
@@ -333,6 +344,10 @@ PRODUCT_PACKAGES += \
     p2p_supplicant_overlay.conf \
     wpa_supplicant_overlay.conf
 
+# Launcher3
+PRODUCT_PACKAGES += \
+    Launcher3
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
@@ -344,7 +359,8 @@ PRODUCT_PACKAGES += \
     wificond \
     wifilogd \
     tcpdump \
-    wcnss_service
+    wcnss_service \
+    libwpa_client
 
 # Wifi Symlinks
 PRODUCT_PACKAGES += \
@@ -367,3 +383,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8996/kernel-headers
