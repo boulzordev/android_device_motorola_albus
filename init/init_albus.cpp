d81124c88d617816327f5c844d5f6cc13f7b058e
Vachounet <vachounet@live.fr>
2017-09-08 13:49:31 +0200

    albus: fix custom init
    Change-Id: I823167e169b3e5ca13734431e70fcd4d810deeb6

--- init/init_albus.cpp
+++ init/init_albus.cpp
@@ -67,7 +67,7 @@ void vendor_load_properties()
         return;
 
     std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
-    property_override("ro.product.model", sku.c_str());
+    property_set("ro.product.model", sku.c_str());
 
     // rmt_storage
     std::string device = android::base::GetProperty("ro.boot.device", "");
